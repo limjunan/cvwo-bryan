69f69941ead974634f4db594ba334c2df52ba136
limjunan <bryanlim080302@gmail.com>
2025-01-21 15:32:03 +0800

    add shadcnui

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Input } from "./ui/input";
 
 const Header: React.FC = () => {
   return (
     <header className=" text-black">
       <div className="container mx-auto py-6">
         <h1 className="text-4xl font-bold">Gossip for CVWO</h1>
+        <Input />
       </div>
       <div className="border-b border-gray-200 "></div>
     </header>
