b2e1821ccdac3972df3c01959f5156ba8e262680
limjunan <bryanlim080302@gmail.com>
2025-01-21 15:59:24 +0800

    create header component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 const Header: React.FC = () => {
   return (
     <header className=" text-black">
-      <div className="container mx-auto py-6">
+      <div className="container mx-auto py-4 flex flex-row items-center justify-between">
         <h1 className="text-4xl font-bold">Gossip for CVWO</h1>
-        <Input />
+        <Input placeholder="Search" className="w-1/2" />
+        <Button className="ml-2" variant="outline">
+          Create Thread
+        </Button>
       </div>
       <div className="border-b border-gray-200 "></div>
     </header>
